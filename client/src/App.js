1f77a6b09c756e2b0ad879a05d245f65f1b6e6eb
Éris <tarkkusss@gmail.com>
2021-04-30 20:48:39 -0300

    Produto minimo viável

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,47 @@
+import "./css/app.css"
+import axios from "axios";
+import {useState, useEffect} from "react";
+
 function App() {
-  return <h1>ola mundo</h1>
-}
+    const [quote, setQuote] = useState("");
+
+    useEffect(() => {
+        randomQuote();
+    }, [])
+
+    const randomQuote = () => {
+        axios.get("/api/search").then(
+            (response) => {
+                setQuote(response.data);
+            }
+        ).catch(err => {
+            console.error(err);
+        })
+    };
+
+    const randomAuthorQuote = autor => {
+        axios.get(`/api/search/${autor}`).then(
+            (response) => {
+                setQuote(response.data);
+            }
+        ).catch(err => {
+            console.error(err);
+        });
+    };
+
+    return (
+        <div className="container">
+            <h1>The Best Quotes of All Time</h1>
+            <p id="quote">{quote}</p>
+            <div className="btns">
+                <button onClick={randomQuote} className="btn">Random quote</button>
+                <button onClick={() => randomAuthorQuote("marx")} className="btn">Marx quote</button>
+                <button onClick={() => randomAuthorQuote("lenin")} className="btn">Lenin quote</button>
+                <button onClick={() => randomAuthorQuote("trotsky")} className="btn">Trotsky quote</button>
+                <button onClick={() => randomAuthorQuote("mao")} className="btn">Mao quote</button>
+            </div>
+        </div>
+    );
+};
 
 export default App;
