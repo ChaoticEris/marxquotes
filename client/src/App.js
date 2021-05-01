5563a57cb81b28098399d8ca5c312693f1dd6a94
Éris <tarkkusss@gmail.com>
2021-04-30 22:51:17 -0300

    Aperfeiçoamento da função de pegar random quote

--- client/src/App.js
+++ client/src/App.js
@@ -9,8 +9,12 @@ function App() {
         randomQuote();
     }, [])
 
-    const randomQuote = () => {
-        axios.get("/api/search").then(
+    const randomQuote = (autor = null) => {
+        let url = "/api/search";
+        if (autor) {
+            url = url + `/${autor}`;
+        }
+        axios.get(url).then(
             (response) => {
                 setQuote(response.data);
             }
@@ -19,29 +23,19 @@ function App() {
         })
     };
 
-    const randomAuthorQuote = autor => {
-        axios.get(`/api/search/${autor}`).then(
-            (response) => {
-                setQuote(response.data);
-            }
-        ).catch(err => {
-            console.error(err);
-        });
-    };
-
     return (
         <div className="container">
             <h1>The Best Quotes of All Time</h1>
             <p id="quote">{quote}</p>
             <div className="btns">
-                <button onClick={randomQuote} className="btn">Random quote</button>
-                <button onClick={() => randomAuthorQuote("marx")} className="btn">Marx/Engels quote</button>
-                <button onClick={() => randomAuthorQuote("lenin")} className="btn">Lenin quote</button>
-                <button onClick={() => randomAuthorQuote("trotsky")} className="btn">Trotsky quote</button>
-                <button onClick={() => randomAuthorQuote("mao")} className="btn">Mao quote</button>
+                <button onClick={() => randomQuote()} className="btn">Random quote</button>
+                <button onClick={() => randomQuote("marx")} className="btn">Marx/Engels quote</button>
+                <button onClick={() => randomQuote("lenin")} className="btn">Lenin quote</button>
+                <button onClick={() => randomQuote("trotsky")} className="btn">Trotsky quote</button>
+                <button onClick={() => randomQuote("mao")} className="btn">Mao quote</button>
             </div>
         </div>
     );
-};
+}
 
 export default App;
